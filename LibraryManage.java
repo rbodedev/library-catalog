8b54de79f6b9f1575e6e173e8fad7d7cd38552bc
rbodedev <bodeip07@gmail.com>
2025-08-24 00:17:42 -0500

    main functionality completed

--- LibraryManage.java
+++ LibraryManage.java
@@ -6,55 +6,97 @@ import java.util.HashMap;
 public class LibraryManage {
 
     public static Map createCatalog (String book) {
-        Map<String, String> dict = new HashMap<>();
+        Map<String, Object> dict = new HashMap<>();
+        Integer onHand = 1;
         String title = book.split(": ")[0];
         String author = book.split(": ")[1];
         dict.put("Title", title);
         dict.put("Author", author);
-        
+        dict.put("quantity", onHand);
         return dict; 
     }
 
-    public static void listTitles (List<Map<String, String>> bookCatalog) {
-        int counter = 1;
+    public static void listTitles (List<Map<String, Object>> bookCatalog) {
         for (int i = 0; i < bookCatalog.size(); i++) {
             
-            Map<String, String> current = bookCatalog.get(i);
-            String title = current.get("Title");
-            String author = current.get("Author");
-            System.out.println(counter + ": Title: " + title + ", Author: " + author);
-            counter++;
+            Map<String, Object> current = bookCatalog.get(i);
+            Object title = current.get("Title");
+            Object author = current.get("Author");
+            Object quantity = current.get("quantity");
+            System.out.println(quantity + " available of " + "Title: " + title + ", Author: " + author);
+
         }
     }
 
-    public static List<Map<String, String>> checkoutBook (List<Map<String, String>> availableBooks, List<Map<String, String>> unavailableBooks, String title) {
-        boolean loopBreak = true;
-        while(true) {
-            for (int i = 0; i < availableBooks.size(); i++) {
-                Map<String, String> current = availableBooks.get(i);
-                if (current.get("Title").equals(title)) {
-                    unavailableBooks.add(availableBooks.get(i));
-                    // availableBooks.remove(availableBooks.get(i));
-                    System.out.println("You are checking out " + title);
-                    loopBreak = false;
-                    return unavailableBooks;
-                }
-            } 
-            for (int i = 0; i < unavailableBooks.size(); i++) {
-                Map<String, String> current = unavailableBooks.get(i);
-                if (current.get("Title").equals(title)) {
-                    System.out.println("Sorry but " + title + " is checked out already.");
-                    loopBreak = false;
-                    List<Map<String, String>> skip = List.of(Map.of("Tile", "skip", "Author", "skip"));
-                    return skip;
+    public static List<Map<String, Object>> checkoutBook (List<Map<String, Object>> bookCatalog, String bookTitle) {
+        for (int i = 0; i < bookCatalog.size(); i++) {
+            Map<String, Object> book = bookCatalog.get(i);
+            if (book.get("Title").equals(bookTitle) && ((Integer) book.get("quantity")) == 0) {
+                System.out.println("Sorry none of that book available");
+            } else if (book.get("Title").equals(bookTitle) && ((Integer) book.get("quantity")) > 0) {
+                int onHandNow = ((Integer) book.get("quantity")) - 1;
+                book.put("quantity", onHandNow);
+                bookCatalog.remove(i);
+                bookCatalog.add(0, book);
+                System.out.println("You have checked out " + bookTitle);
+            } else {
+                continue;
+            }
+        }
+        return bookCatalog;
+    }
+
+    public static List<Map<String, Object>> removeBook (List<Map<String, Object>> bookCatalog, String bookTitle) {
+        for (int i = 0; i < bookCatalog.size(); i++) {
+            Map<String, Object> book = bookCatalog.get(i);
+            if (book.get("Title").equals(bookTitle)) {
+                bookCatalog.remove(i);
+                System.out.println(bookTitle + " removed!");
+                return bookCatalog;
+            } else {
+                continue;
+            }
+        }
+        return bookCatalog;
+    }
+
+    public static void searchBook (List<Map<String, Object>> bookCatalog, String search) {
+        
+        for (int i = 0; i < bookCatalog.size(); i++) {
+            Map<String, Object> book = bookCatalog.get(i);
+            
+            String currentString;
+
+            currentString = ((String) book.get("Title"));
+            String[] words = currentString.split(" ");
+            for (int j = 0; j < words.length; j++) {
+                if (words[j].equals(search)) {
+                    System.out.print("Found: " + book);
                 }
             }
-            System.out.println("Incorrect title, please re-enter");
-            List<Map<String, String>> skip = List.of(Map.of("Tile", "skip", "Author", "skip"));
-            return skip;
-
         }
 
-
     }
+
+    public static List<Map<String, Object>> returnBook (List<Map<String, Object>> bookCatalog, String bookTitle) {
+        for (int i = 0; i < bookCatalog.size(); i++) {
+            Map<String, Object> book = bookCatalog.get(i);
+            if (book.get("Title").equals(bookTitle)) {
+                int onHandNow = ((Integer) book.get("quantity"));
+                onHandNow++;
+                book.put("quantity", onHandNow);
+                bookCatalog.remove(i);
+                bookCatalog.add(0, book);
+                System.out.println(book.get("Title") + " has been returned!");
+            } else {
+                continue;
+            }
+        }
+        return bookCatalog;
+    }
+
+
+
+
+
 }
--- LibraryManagementDemo.java
+++ LibraryManagementDemo.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.io.*;
 import java.util.Map;
 import java.util.List;
+import java.util.HashMap;
 
 
 public class LibraryManagementDemo {
@@ -12,50 +13,96 @@ public class LibraryManagementDemo {
         String filename = "books.txt"; 
         
         List<Map<String, Object>> bookCatalog = new ArrayList<>();
-        List<Map<String, Object>> availableBooks = new ArrayList<>();
-        List<Map<String, String>> unavailableBooks = new ArrayList<>();
-        List<Map<String, String>> intermediate = new ArrayList<>();
-        List<Map<String, String>> skip = List.of(Map.of("Tile", "skip", "Author", "skip"));
 
         File file = new File(filename);
-        System.out.println(file);
+        // System.out.println(file);
         Scanner booksList = new Scanner(file);
         Scanner userIn = new Scanner(System.in);
-        
+        Scanner librarianIn = new Scanner(System.in);
+
         while (booksList.hasNext()) {
             String titleAuthor;
             titleAuthor = booksList.nextLine();
-            System.out.println(titleAuthor);
+            // System.out.println(titleAuthor);
             bookCatalog.add(LibraryManage.createCatalog(titleAuthor));
-            availableBooks.add(LibraryManage.createCatalog(titleAuthor));
-        }
-
-        System.out.println(bookCatalog);
-
-        boolean loopBreak = true;
-
-        while(loopBreak == true) {
-            System.out.print("testtest: ");
-            String input = userIn.nextLine();
-            System.out.println("testtest");
-            if (input.equals("false")) {
-                loopBreak = false;
-            }
-
+            // availableBooks.add(LibraryManage.createCatalog(titleAuthor));
         }
 
         LibraryManage.listTitles(bookCatalog);
 
-        intermediate = LibraryManage.checkoutBook(availableBooks, unavailableBooks, "Hairy Ponder and the Rock");
-        if (intermediate.equals())
-        unavailableBooks = LibraryManage.checkoutBook(availableBooks, unavailableBooks, "Hairy Ponder and the Rock");
-        availableBooks.removeAll(unavailableBooks);
-        LibraryManage.listTitles(availableBooks);
-        LibraryManage.listTitles(unavailableBooks);
+        boolean loopBreak = true;
 
+        while(loopBreak == true) {
+            System.out.print("please enter the title of the book you want to checkout or enter false to leave: ");
+            String input = userIn.nextLine();
+            bookCatalog = LibraryManage.checkoutBook(bookCatalog, input);
+            System.out.println();
+            if (input.equals("false")) {
+                loopBreak = false;
+            }
+            String librarianCheck = input.toLowerCase();
+            if (librarianCheck.equals("librarian")) {
+                System.out.print("Please enter password: ");
+                input = librarianIn.nextLine();
+                if (input.equals("pw")) {
+                    String command;
+                    System.out.println("Librarian mode activated");
+                    System.out.println("In this mode you can enter command types: ADD, CHECKIN, REMOVE, SEARCH, LIST, EXIT");
+                    System.out.print("Please enter your command type: ");
+                    command = librarianIn.nextLine();
+                    command = command.toLowerCase();
+                    if (command.equals("add")) {
+                        Map<String, Object> dict = new HashMap<>();
+                        System.out.print("Please enter the new book title: ");
+                        String title = librarianIn.nextLine();
+                        System.out.print("Please enter the new book author: ");
+                        String author = librarianIn.nextLine();
+                        System.out.print("Please enter the new book quantity: ");
+                        int quantity = librarianIn.nextInt();
+                        dict.put("Title", title);
+                        dict.put("Author", author);
+                        dict.put("quantity", quantity);
+                        bookCatalog.add(dict);
+                        System.out.println("New library catalog:");
+                        LibraryManage.listTitles(bookCatalog);
+                        System.out.println("librarian mode deactivated for security");
+
+                    } else if (command.equals("remove")) {
+                        System.out.print("Please enter the title of the book to remove: ");
+                        String title = librarianIn.nextLine();
+                        bookCatalog = LibraryManage.removeBook(bookCatalog, title);
+                        System.out.println("librarian mode deactivated for security");
+
+                    } else if (command.equals("search")) {
+                        System.out.print("Please enter a word from the title or author and a search will be conducted: ");
+                        String search = librarianIn.nextLine();
+                        LibraryManage.searchBook(bookCatalog, search);
+
+                        System.out.println("librarian mode deactivated for security");
+
+                    }else if (command.equals("list")) {
+                        LibraryManage.listTitles(bookCatalog);
+                        System.out.println("librarian mode deactivated for security");
+                    } else if (command.equals("exit")) {
+                        loopBreak = false;
+                        System.out.println("librarian mode deactivated for security");
+                    } else if (command.equals("checkin")) {
+                        System.out.print("Please enter the title of the book to be returned: ");
+                        String returning = librarianIn.nextLine();
+                        bookCatalog = LibraryManage.returnBook(bookCatalog, returning);
+                        System.out.println();
+                    } else {
+                        continue;
+                    }
+
+                } else {
+                    System.out.println("Incorrect password, client mode returned");
+                }
+
+            }
+
+        }
 
     }
-
-
 
 }
